08336e241a1c516359c6f610af70635934538f4c
chenlaug <94695334+chenlaug@users.noreply.github.com>
2023-05-31 16:55:54 +0200

    add table and button

---
+++ front-end/src/page/Rh.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Table from '../components/Table';
+
+export default function Rh() {
+  return (
+    <Box>
+      <Table />
+    </Box>
+  );
+}
