c435a17536f64255999086c81a9bd3b358a05837
chenlaug <94695334+chenlaug@users.noreply.github.com>
2023-06-12 10:21:13 +0200

    Add table

---
+++ front-end/src/components/Tableau/Tableau.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { rows } from './row';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
+
+export default function Tableau() {
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handleClickAdd = () => {
+    alert("Button ajout d'utilisateur");
+  };
+
+  const handleClickeDelete = () => {
+    console.log('les id : ', selectedRows);
+  };
+
+  const handleClickEdit = (userId) => {
+    alert("Modifier l'utilisateur avec l'ID : " + userId);
+  };
+
+  const Edit = (params) => {
+    const userId = params.row.id;
+
+    const handleEditClick = () => {
+      handleClickEdit(userId);
+    };
+
+    return (
+      <strong>
+        <Button variant="contained" onClick={handleEditClick}>
+          Edit
+        </Button>
+      </strong>
+    );
+  };
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 230,
+    },
+    {
+      field: 'telephone',
+      headerName: 'Téléphone',
+      type: 'number',
+      width: 150,
+    },
+    {
+      field: 'poste',
+      headerName: 'Poste',
+      width: 190,
+    },
+    {
+      field: 'salaire',
+      headerName: 'Salaire',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+    { field: 'edit', headerName: 'Edit', renderCell: Edit },
+  ];
+
+  return (
+    <div>
+      <Box sx={{ display: 'flex', gap: 1 }}>
+        <Button variant="contained" color="primary" onClick={handleClickAdd}>
+          + Ajouter
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleClickeDelete}
+        >
+          - Supprimer sélectionnés
+        </Button>
+      </Box>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        checkboxSelection
+        onRowSelectionModelChange={(itm) => setSelectedRows(itm)}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        pageSizeOptions={[5, 10, 50]}
+      />
+    </div>
+  );
+}
---
+++ front-end/src/page/RhHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Table from '../components/Tableau/Tableau';
+
+export default function RhHome() {
+  return (
+    <Box>
+      <Table />
+    </Box>
+  );
+}
