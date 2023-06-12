b54f39293afb0c1860e29b7bbd4ca56ab1c02804
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-06-12 10:25:31 +0200

    Backend: add AddAbsence and Validate api method

--- back-end/resolvers/absence-resolvers.js
+++ back-end/resolvers/absence-resolvers.js
@@ -4,10 +4,28 @@ const absencesResolvers = {
     Query: {
         getAbsence: async (parent, { id }, context, info) => {
             const absence = await database.select().from("absence").where("id", id)
-            console.log(absence)
             return absence[0]
+        },
+        getAbsences: async (parent, args, context, info) => {
+            return await database.select().from('absence');
         }
     },
+    Mutation: {
+        validate: async (parent, { id }, context, info) => {
+            const absence = await database.select().from("absence").where("id", id)
+            await database("absence").where("id", id).update("valide", absence[0].valide ? 0 : 1)
+            return "Validation state have been updated successfully"
+        },
+        addAbsence: async (parent, { employe_id, date_debut, date_fin }, context, info) => {
+            const absence = {
+                "employe_id": employe_id,
+                "date_debut": date_debut,
+                "date_fin": date_fin
+            }
+            await database("absence").insert(absence);
+            return "Absence Created"
+        }
+    }
 };
 
 export default absencesResolvers;
--- back-end/schemas/absence-schemas.js
+++ back-end/schemas/absence-schemas.js
@@ -6,7 +6,13 @@ const absencesSchemas = gql `
     scalar Date
 
     type Query {
-        getAbsence(id: Int!): Absence 
+        getAbsence(id: Int!): Absence!
+        getAbsences: [Absence!]
+    }
+
+    type Mutation {
+        addAbsence(employe_id: Int!, date_debut: Date!, date_fin: Date!): String!
+        validate(id: Int!): String!
     }
 
     type Absence {
