eff45149df40620abcb9bea8c74f7844c27c79c2
Vyvy20 <c.dang2001@gmail.com>
2023-06-12 22:32:40 +0200

    Add some components to add and delete a holiday

--- front-end/src/components/EmployeeProfil/Profil.js
+++ front-end/src/components/EmployeeProfil/Profil.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {Box, Button, TextField, Typography, styled, Table, TableBody, TableCell, TableContainer, TableRow, TableHead} from '@mui/material';
+import {Box, Button, TextField, Typography, Table, TableBody, TableCell, TableContainer, TableRow, TableHead, Grid, MenuItem} from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 //Table
 function createData(startDateDonn, endDateDonn, type, status, action) {
@@ -7,12 +10,24 @@ function createData(startDateDonn, endDateDonn, type, status, action) {
 }
 
 const rows = [
-  createData('12/05/2023', '19/05/2023', 'Congé payé', 'Validé', <Button color='inherit'>Modifier</Button>),
+  createData('12/05/2023', '19/05/2023', 'Congé payé', 'Validé', <Box><Button color='inherit' variant="outlined">Modifier</Button> <Button color='inherit' variant="outlined">Supprimer</Button></Box>),
+];
+
+//Type de vacances
+const typeHol = [
+  {
+    value: "paidLeave",
+    label: "Congé payé"
+  },
+  {
+    value: "unpaidLeave",
+    label: "Congé sans solde"
+  },
 ];
 
 function Profil() {
 
-    return(
+  return(
         <Box>
             <Box>
                 <Typography variant='h3'>Mon profil</Typography>
@@ -35,23 +50,45 @@ function Profil() {
                       </TableRow>
                     </TableHead>
                     <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.startDateDonn}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.startDateDonn}
-              </TableCell>
-              <TableCell align="left">{row.endDateDonn}</TableCell>
-              <TableCell align="left">{row.type}</TableCell>
-              <TableCell align="left">{row.status}</TableCell>
-              <TableCell align="left">{row.action}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.startDateDonn}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">{row.startDateDonn}</TableCell>
+                        <TableCell align="left">{row.endDateDonn}</TableCell>
+                        <TableCell align="left">{row.type}</TableCell>
+                        <TableCell align="left">{row.status}</TableCell>
+                        <TableCell align="left">{row.action}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
                   </Table>
                 </TableContainer>
+                <Grid container>
+                  <Grid item xs={3}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker required label="Date de début"/>
+                    </LocalizationProvider>
+                  </Grid>
+                  <Grid item xs={3}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker required label="Date de fin"/>
+                    </LocalizationProvider>
+                  </Grid>
+                  <Grid item xs={2}>
+                    <TextField required select label="Type de congé" variant="outlined">
+                      {typeHol.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Button color='inherit' variant="outlined">Ajouter</Button>
+                  </Grid>
+                </Grid>        
             </Box>
             <Box>
                 <Typography variant='h3'>Ma fiche de paie</Typography>
