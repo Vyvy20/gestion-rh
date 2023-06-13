6fa1abbe0002e4368de88b84dd60d7ac83cf032a
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-06-13 17:15:42 +0200

    frontend: add a middleware to communicate token to back

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { unstable_HistoryRouter as HistoryRouter , useRoutes } from "react-router-dom";
-import { ApolloProvider, InMemoryCache, ApolloClient } from "@apollo/client";
+import { ApolloProvider, InMemoryCache, ApolloClient, ApolloLink, concat, HttpLink } from "@apollo/client";
 import { createBrowserHistory } from 'history';
 import MAP_ROUTES from "./routes";
 
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || null,
+    }
+  }));
+  return forward(operation);
+})
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
-  headers: {
-    authorization: localStorage.getItem("token")
-  }
+  link: concat(authMiddleware, httpLink),
 });
 
 function AppRoutes() {
