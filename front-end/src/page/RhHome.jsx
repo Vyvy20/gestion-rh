ae3a28a7949612299ddfc8dc2240c25e1c228ebb
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-06-13 15:07:40 +0200

    RHHome: add role authorization

--- front-end/src/page/RhHome.jsx
+++ front-end/src/page/RhHome.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from "./Interface.js"
 import Box from '@mui/material/Box';
 import Table from '../components/Tableau/Tableau';
+import { Typography } from '@mui/material';
 
 export default function RhHome() {
+  const user = useContext(UserContext)
+
+  if (!user) {
+    return <Typography>Connect before you can access this page</Typography>
+  }
+
+  if (user.role !== "rh") {
+    return <Typography>You don't have the authorization to access this page</Typography>
+  }
+
   return (
     <Box>
       <Table />
