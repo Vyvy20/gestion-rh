9165c009397be1fc567270ac2ec3959c08317ee8
Vyvy20 <c.dang2001@gmail.com>
2023-06-14 16:14:07 +0200

    Add paystub management

--- front-end/src/components/RhProfil/rhProfil.js
+++ front-end/src/components/RhProfil/rhProfil.js
@@ -16,6 +16,10 @@ function createData3(startDateDonn2, endDateDonn2, addrEmail, status2) {
   return { startDateDonn2, endDateDonn2, addrEmail, status2 };
 }
 
+function createData4(paystub2, addrEmail2, action3) {
+  return { paystub2, addrEmail2, action3 };
+}
+
 //Créer une donnée test
 const rows = [
   createData('12/05/2023', '19/05/2023', 'Validé', <Box><Button color='inherit' variant="outlined">Modifier</Button> <Button color='inherit' variant="outlined">Supprimer</Button></Box>),
@@ -33,6 +37,14 @@ function RhProfil() {
   const rows2 = [
     createData2("fiche_paie_janvier_2023", <Box><Button color='inherit' variant="outlined" onClick={handleClick} href={fiche ? fiche2:!fiche2}>Voir</Button></Box>),
   ];
+
+  const fiche3 = new URL("../../images/Fiche_paie_mai.jpg", import.meta.url)
+  const [fiche4, setFiche2] = useState(new URL(fiche3, import.meta.url))
+  const handleClick2 = () => {setFiche2(new URL("http://localhost:3000/app/home", import.meta.url))}
+  const rows4 = [
+    createData4("fiche_paie_mai_2023", "daniel.dupont@anchorink.com", <Box><Button color='inherit' variant="outlined" onClick={handleClick2} href={fiche4 ? fiche3:!fiche3}>Voir</Button> <Button color='inherit' variant="outlined">Modifier</Button> <Button color='inherit' variant="outlined">Supprimer</Button></Box>),
+  ];
+
   const [firstName, setFirstName] = useState("")
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
@@ -41,6 +53,7 @@ function RhProfil() {
   const [pwd, setPwd] = useState("")
   const [job, setJob] = useState("")
   const [tel, setTel] = useState("")
+  const [email2, setEmail2] = useState("")
 
   return(
         <Box>
@@ -164,6 +177,38 @@ function RhProfil() {
                     </TableBody>
                 </Table>
               </TableContainer>
+            </Box>
+            <Box>
+              <Typography variant='h3'>Gestion des fiches de paie</Typography>
+              <TableContainer>
+              <TableHead>
+                  <TableRow>
+                    <TableCell align='left'>Fiche de paie</TableCell>
+                    <TableCell align='left'>Adresse email</TableCell>
+                    <TableCell align='left'>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows4.map((row4) => (
+                    <TableRow
+                      key={row4.paystub2}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">{row4.paystub2}</TableCell>
+                      <TableCell align='left'>{row4.addrEmail2}</TableCell>
+                      <TableCell align="left">{row4.action3}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </TableContainer>
+              <Grid container>
+                <Grid item xs={4}>
+                  <TextField type="file" variant='outlined'>Bulletin de salaire</TextField>
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField label="Adresse mail" variant='outlined' onChange={e => setEmail2(e.target.value)}  value={email2}></TextField>
+                </Grid>
+              </Grid>
             </Box>
         </Box>
     )
