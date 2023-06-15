38fe96327096308e33ad003ed240fd9461124624
chenlaug <94695334+chenlaug@users.noreply.github.com>
2023-06-15 12:11:39 +0200

    remove conflicts

--- front-end/src/api/employeApi.js
+++ front-end/src/api/employeApi.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
-// Query
-export const GET_EMPLOYES = gql`
-  query Query {
-    getEmployes {
+
+export const GET_EMPLOYE = gql`
+  query Query($employeId: Int!) {
+    getEmploye(id: $employeId) {
       id
       nom
       prenom
@@ -13,11 +13,43 @@ export const GET_EMPLOYES = gql`
       jours
       joursRestant
       joursPrit
+      role
     }
   }
 `;
 
-// Mutation
+export const UPDATE_EMPLOYE = gql`
+  mutation Mutation(
+    $id: Int!
+    $prenom: String
+    $nom: String
+    $email: String
+    $telephone: String
+  ) {
+    updateEmploye(
+      id: $id
+      prenom: $prenom
+      nom: $nom
+      email: $email
+      telephone: $telephone
+    )
+  }
+`;
+
+export const UPDATE_PASSWORD = gql`
+  mutation Mutation(
+    $id: Int!
+    $currentPassword: String!
+    $newPassword: String!
+  ) {
+    changePassword(
+      id: $id
+      currentPassword: $currentPassword
+      newPassword: $newPassword
+    )
+  }
+`;
+
 export const ADD_EMPLOYE = gql`
   mutation AddEmploye(
     $prenom: String!
@@ -51,43 +83,5 @@ export const DELETE_EMPLOYE = gql`
 export const DELETES_EMPLOYES = gql`
   mutation DeleteEmployes($ids: [Int!]) {
     deleteEmployes(ids: $ids)
-  }
-`;
-
-export const UPDATE_EMPLOYES = gql`
-  mutation UpdateEmploye(
-    $updateEmployeId: Int!
-    $jours: Int!
-    $prenom: String
-    $nom: String
-    $email: String
-    $telephone: String
-    $poste: String
-    $salaire: Int
-  ) {
-    updateEmploye(
-      id: $updateEmployeId
-      jours: $jours
-      prenom: $prenom
-      nom: $nom
-      email: $email
-      telephone: $telephone
-      poste: $poste
-      salaire: $salaire
-    )
-  }
-`;
-
-export const UPDATE_PASSWORD = gql`
-  mutation Mutation(
-    $id: Int!
-    $currentPassword: String!
-    $newPassword: String!
-  ) {
-    changePassword(
-      id: $id
-      currentPassword: $currentPassword
-      newPassword: $newPassword
-    )
   }
 `;
