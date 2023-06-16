d0a5bc4271667b19cd5c208dc51a0ab7d1a001cb
Esteban Gonzalez <esteban.frisco.gonzalez@gmail.com>
2023-06-16 14:15:23 +0200

    update bug

--- back-end/helpers/employeHelper.js
+++ back-end/helpers/employeHelper.js
@@ -1,3 +1,5 @@
+import { database } from "../database.js";
+
 export async function joursRestant(employe_id) {
     const results = await database.select("duree").from("absence").where("employe_id", employe_id)
     let duree = 0;
